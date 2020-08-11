c7342e670888f3941fd32ce2c06f9814d2cd38ef
boyloe <boyloe@gmail.com>
2020-08-11 08:41:31 -0600

    Solve using enumerable .select

--- index.rb
+++ index.rb
@@ -3,3 +3,10 @@ def opposite (num)
     num * -1
    end
 ##``   
+
+#8-11-2020 "Counting Sheep" -Given an array where true means as sheep is present, count the number of sheep.
+def countSheeps array
+    array.select do |sheep|
+      sheep == true
+    end.length  
+end
