6014da4b037a01256a0ae1b1a4945173367dd7aa
boyloe <boyloe@gmail.com>
2020-08-31 09:08:03 -0600

    Solve 7kyu - Categorize New Member

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function productFib(prod){
     }
     return [n, nNext, n*nNext==prod];
 }
+
+//08-31-2020 '7kyu - Categorize New Member'
+function openOrSenior(data){
+    membership = [];
+    data.map(prospect => 
+            {if(prospect[0] > 54 && prospect[1] > 7) {
+                membership.push("Senior");
+            }else{
+                membership.push("Open");
+    }
+  })
+  return membership
+}
+let test1 = [[57,12],[54, 12], [59,6]]
+console.log(openOrSenior(test1));
