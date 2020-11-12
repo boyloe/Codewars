0b4465c9ae697ba5566d183c634fbf8398b64a86
boyloe <boyloe@gmail.com>
2020-11-12 08:30:20 -0700

    Solve Stop gninnipS My sdroW!

--- index.ts
+++ index.ts
@@ -8,3 +8,17 @@ export function findNeedle(haystack: any[]):string {
     const needleIndex:number = haystack.findIndex((element:any) => element === "needle")
     return `found the needle at position ${needleIndex}`
 }
+
+//11-12-2020 'Stop gninnipS My sdroW!'
+export class Kata {
+    static spinWords(words: string) {
+        const wordsArr:string[] = words.split(' ')
+        const spunArr:string[] = wordsArr.map((word:string) => {
+            if (word.length > 4) {
+            return word.split('').reverse().join('')
+            }
+            return word
+        })
+        return spunArr.join(' ')
+    }
+}
