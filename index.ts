2c0dc17ad0e1a6a4a14f9c3a27f72633a6293a89
boyloe <boyloe@gmail.com>
2020-11-12 08:29:03 -0700

    Solve Needle in a haystack

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export function findSmallestInt(args: number[]): number {
     return args.sort((a:number, b:number)=> a-b)[0]
 }
 
+//11-10-2020 'Needle in a haystack'
 export function findNeedle(haystack: any[]):string {
     const needleIndex:number = haystack.findIndex((element:any) => element === "needle")
     return `found the needle at position ${needleIndex}`
