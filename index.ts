3c3026428d809d37937f30a4e5af375c4bb9b969
boyloe <boyloe@gmail.com>
2020-11-27 10:12:31 -0500

    Correct bad variable name strarr to strArr

--- index.ts
+++ index.ts
@@ -151,7 +151,7 @@ export class G964 {
 //   }
 
 //11-26-2020 '6-kyu Consecutive Strings'
-export function longestConsec(strarr: string[], k: number): string {
+export function longestConsec(strArr: string[], k: number): string {
     return "solution"
 }
 
